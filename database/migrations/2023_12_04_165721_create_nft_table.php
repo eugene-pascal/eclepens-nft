80d0a1a0906676389a49811a6dad8950247b730c
Yevgenii S. aka mrPascal <me@mrpascal.com>
2023-12-04 18:19:11 +0100

    added a new table nft

---
+++ database/migrations/2023_12_04_165721_create_nft_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNftTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nft', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug')->nullable()->unique();
+            $table->string('standard', 32);
+            $table->string('author', 32);
+            $table->string('name',255);
+            $table->longText('descr')->nullable();
+            $table->boolean('display')->index();
+            $table->dateTime('published_at')->useCurrent()->index();
+            $table->unsignedSmallInteger('max_nft')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nft');
+    }
+}
